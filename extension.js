859c00a80e3bb35c8598e5cd67eb27c0857e5e76
DartRuffian <skulldude327@gmail.com>
2024-12-29 20:12:27 -0600

    Wip

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
-// The module 'vscode' contains the VS Code extensibility API
+// The module "vscode" contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -9,22 +9,14 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+    const copyMacroPath = vscode.commands.registerCommand("arma-vscode.copyMacroPath", async function (editor) {
+        let macroPath = editor.path;
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "arma-vscode" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    const disposable = vscode.commands.registerCommand('arma-vscode.helloWorld', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        vscode.window.showInformationMessage(`Copied ${macroPath} path to clipboard`);
+        await vscode.env.clipboard.writeText(macroPath);
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(copyMacroPath);
 }
 
 // This method is called when your extension is deactivated
